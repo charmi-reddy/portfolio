367f142c4e9b017eaa4b1e0f5e044c2346f4822e
charmi-reddy <23ra1a6620@kpritech.ac.in>
2026-02-28 15:27:28 +0530

    Edited the year name

--- script.js
+++ script.js
@@ -194,8 +194,7 @@ const apps = {
                 </section>
 
                 <footer class="pt-20 border-t border-purple-900/20 flex flex-col md:flex-row justify-between items-center gap-4 text-[10px] mono uppercase tracking-[0.2em] text-purple-900">
-                    <span>AI_ML_PORTFOLIO_2024</span>
-                    <span>© 2024 / ALL RIGHTS RESERVED</span>
+                    <span>© 2026 ALL RIGHTS RESERVED.</span>
                 </footer>
                 
                 ${getNavButtons('home').html}
